7eecbfc58ca922b573ab497ed2c7a6cb2ade6480
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-05-08 22:30:32 -0400

    starting on profile page and route

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+
+const PostPage: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Posty~!</title>
+        <meta name="description" content="📜" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex justify-center h-screen">
+    
+      <h2>Post View</h2>
+
+      </main>
+    </>
+  );
+};
+
+export default PostPage;
