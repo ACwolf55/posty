a34b545b27446ab48d39750b417aed797e71212a
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-05-17 17:16:38 -0400

    ssghelper needs fix, pagelayout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import { RouterOutputs, api } from "~/utils/api";
 
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
+import { PageLayout } from "~/components/PageLayout";
 
 dayjs.extend(relativeTime)
 
@@ -153,18 +154,15 @@ const Home: NextPage = () => {
         <meta name="description" content="📜" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex justify-center h-screen">
-      
-      <div className="h-full w-full md:max-w-2xl border-x border-slate-400">
-    <div className="flex border-b border-slate-400 p-4">
+      <PageLayout>
+      <div className="flex border-b border-slate-400 p-4">
       {!isSignedIn && 
       <div className="flex justify-center"><SignInButton /></div>}
       {isSignedIn && <CreatePostWizard />}
       </div>
       <Feed />
-    </div>
+     </PageLayout>
 
-      </main>
     </>
   );
 };
