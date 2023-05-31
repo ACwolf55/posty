5d4c0d6c647212e3475fa7f51cead460a38c9c23
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-05-31 18:16:36 -0400

    profile pic change for deploy

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -44,8 +44,8 @@ const ProfilePage: NextPage<{ username: string}> = ({username}) => {
       <PageLayout>
       <div className="relative h-36 bg-slate-600">
           <Image
-            src={data.profileImageUrl}
-            alt={data.profilePicture}
+            src={data.profilePicture}
+            alt=''
             width={128}
             height={128}
             className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
